9bcf2fb3d524fab92cffbd1233317adedc48707a
Juho Alasalmi <juho.alasalmi@ptt.fi>
2021-02-04 09:40:20 +0100

    add options to get keys as named vectors

--- R/get_occupationkey.R
+++ R/get_occupationkey.R
@@ -9,16 +9,22 @@
 #' @examples
 #'
 #'    get_occupationkey(1)
+#'    get_occupationkey(1, as_named_vector = TRUE)
 #'
-get_occupationkey <- function(code_level = NULL, year = 2010) {
+get_occupationkey <- function(code_level = NULL, year = 2010, as_named_vector = FALSE) {
 
-  localId <- search_series("ammatti", year = year, as_localId = TRUE)
-  series <- get_series(localId, print_series_name = FALSE)
+  localId <- search_classifications("ammatti", year = year, as_localId = TRUE)
+  classif <- get_classification(localId, print_series_name = FALSE)
 
   if(!is.null(code_level)) {
-    series <- dplyr::filter(series, nchar(code) == code_level)
+    classif <- dplyr::filter(classif, nchar(code) == code_level)
+  }
+  output <- classif
+  if(as_named_vector) {
+    output <- classif$name
+    names(output) <- classif$code
   }
 
-  series
+  output
 
 }
